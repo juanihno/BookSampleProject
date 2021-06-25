8a84aac7b26da60c08a64129c9134ebb18c1244d
johannes <jmuljana@gmail.com>
2021-06-25 11:36:52 +1000

    adding routes in content

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,7 +1,20 @@
+import { Switch, Route } from 'react-router-dom';
+
+import {Home} from './Home';
+import {About} from './About';
+
 export function Content( props ) {
   return(
     <div className="container">
       <h1>Hello Content</h1>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
     </div>
   )
 }
