8504464b8116a036ac359f7c14a027d1561e6e0d
johannes <jmuljana@gmail.com>
2021-06-25 10:43:53 +1000

    first commit

---
+++ src/Components/Content.js
@@ -0,0 +1,7 @@
+export function Content( props ) {
+  return(
+    <div className="container">
+      <h1>Hello Content</h1>
+    </div>
+  )
+}
---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+export function Footer( props ) {
+  return(
+    <footer className="footer">
+      <div className="container-fluid">
+        <h1>Hello Footer</h1>
+      </div>
+    </footer>
+  )
+}
