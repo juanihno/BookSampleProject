ecf66b1714b98179180e7512a5e9d81f658ab2c7
johannes <jmuljana@gmail.com>
2021-07-09 10:31:02 +1000

    adding autoloader and Database class for php

---
+++ public/php/src/Database.php
@@ -0,0 +1,28 @@
+<?php
+namespace bookstore;
+
+class Database {
+  private $host;
+  private $username;
+  private $password;
+  private $databasename;
+  private $connection;
+
+  protected function __construct() {
+    $this -> host = "localhost";
+    $this -> username = "website";
+    $this -> password = "password";
+    $this -> databasename = "Heartbooks";
+    $this -> connection = mysqli_connect(
+      $this -> host,
+      $this -> username,
+      $this -> password,
+      $this -> databasename
+    );
+  }
+
+  protected function getConnection() {
+    return $this -> connection;
+  }
+}
+?>
