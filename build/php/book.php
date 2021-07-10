d8863c7f166d6e7750cff033dfd98e0c3729b782
johannes <jmuljana@gmail.com>
2021-07-10 17:02:33 +1000

    error checking

--- build/php/book.php
+++ build/php/book.php
@@ -1,14 +1,17 @@
 <?php
 require('vendor/autoload.php');
 
-use bookstore\Books;
+// use bookstore\Books;
 
-$books = new Books();
+// $books = new Books();
 
-$books_results = $books -> getBooks();
+// $books_results = $books -> getBooks();
 
-//print_r( $books_results);
+// //print_r( $books_results);
 
-echo json_encode( $books_results );
+// echo json_encode( $books_results );
+
+$conn = mysqli_connect("localhost", "website", "password", "heartbooks");
+print_r( $conn );
 
 ?>
