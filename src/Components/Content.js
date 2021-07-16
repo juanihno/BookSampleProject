0d3607012ecfb900d8e75808611ade8465a9664e
Juan <jjjbaque@gmail.com>
2021-07-16 11:31:54 +1000

    class7

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,18 @@
 import { Switch, Route } from 'react-router-dom';
+import { firebaseConfig } from '../config/Config';
+import  firebase  from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/firestore';
+
 
 import {Home} from './Home';
 import {About} from './About';
 
 export function Content( props ) {
+  if (!firebase.apps.length){
+    firebase.initializeApp( firebaseConfig);
+  }
   return(
     <div className="container">
       <Switch>
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_mICW-nMu4zU2vvnC8wJup5F5zK_oBtc",
+    authDomain: "booksdemo-6a67b.firebaseapp.com",
+    projectId: "booksdemo-6a67b",
+    storageBucket: "booksdemo-6a67b.appspot.com",
+    messagingSenderId: "373471048649",
+    appId: "1:373471048649:web:191b55c077d5453ada2e18"
+  };
